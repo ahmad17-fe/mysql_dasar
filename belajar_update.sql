b88fed3cf631f4a0315e074f496f4f287a48543e
ahmad17-fe <ahmads1704@gmail.com>
2024-08-18 12:54:15 +0700

    feat: learn update in mysql

---
+++ belajar_update.sql
@@ -0,0 +1,59 @@
+SHOW DATABASES;
+
+USE
+  belajar_mysql;
+
+ALTER TABLE
+  products
+ADD COLUMN
+  category ENUM('Makan', 'Minuman', 'Lain-lain') AFTER name;
+
+ALTER TABLE
+  products
+MODIFY
+  category ENUM('Makanan', 'Minuman', 'Lain-Lain');
+
+DESC products;
+
+UPDATE
+  products
+SET
+  category = 'Makanan'
+WHERE
+  id = 'P0001';
+
+UPDATE
+  products
+SET
+  description = 'Mie Ayam Originial dengan toping bakso tahu'
+WHERE
+  id = 'P0002';
+
+UPDATE
+  products
+SET
+  category = 'Makanan'
+WHERE
+  id = 'SALAH';
+
+UPDATE
+  products
+SET
+  category = 'Makanan',
+  description = 'Mie Ayam dengan toping tambahan ceker'
+WHERE
+  id = 'P0003';
+  
+UPDATE 
+	products
+SET
+	price = price + 5000
+WHERE
+	id = 'P0005'
+
+SELECT
+  *
+FROM
+  products;
+
+SELECT * FROM products WHERE category IS NULL;
